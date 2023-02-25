79a4e8d3a645434f10951e3a507313636d70515f
Ivan Fuentes <ivantxp@gmail.com>
2023-02-25 14:53:17 -0300

    s

--- js/js.js
+++ js/js.js
@@ -1,3 +1,21 @@
 let a = dayjs().startOf('year')
+let gastos = [] 
 
-console.log(a)
+if( localStorage.getItem("gastosUsuario") != null){
+    gastos = JSON.parse(localStorage.getItem("gastosUsuario"));
+    console.log(gastos)
+}
+
+dayjs.locale("es")
+let tituloGasto= document.getElementById("titulo_gasto")
+let ingresoFecha = document.getElementById("fecha_ingreso")
+let credorGasto = document.getElementById("btn_crear")
+credorGasto.onclick = crearEveto
+
+
+function crearEveto(){
+    gastos.push({id: gastos.length + 1, titulo: tituloGasto.value, fecha:ingresoFecha.value})
+    console.log(gastos)
+    alert(dayjs(gastos[gastos.length-1].fecha).format('DD-MMMM-YYYY'))
+    localStorage.setItem("gastosUsuario",JSON.stringify(gastos));
+}
