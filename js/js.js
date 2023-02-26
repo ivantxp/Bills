82efbf0291d71cebd0c92bceb4b65a2056d5ac02
ivantxp <ivantxp@gmail.com>
2023-02-26 13:55:41 -0300

    d

--- js/js.js
+++ js/js.js
@@ -1,27 +1,56 @@
-let a = dayjs().startOf('year')
-let gastos = [] 
+dayjs.locale("es");
+let gastos = [] ;
+let visualizacionGastos = document.getElementById("visualizacion_gastos");
+let tituloGasto= document.getElementById("titulo_gasto");
+let ingresoFecha = document.getElementById("fecha_ingreso");
+let credorGasto = document.getElementById("btn_crear");
+credorGasto.onclick = crearEveto;
+
+let montoGastado = document.getElementById("monto_gastado");
+
+
+
 
 if( localStorage.getItem("gastosUsuario") != null){
     gastos = JSON.parse(localStorage.getItem("gastosUsuario"));
-    console.log(gastos)
+    console.log(gastos);
 }
 
-dayjs.locale("es")
-let tituloGasto= document.getElementById("titulo_gasto")
-let ingresoFecha = document.getElementById("fecha_ingreso")
-let credorGasto = document.getElementById("btn_crear")
-credorGasto.onclick = crearEveto
+function renderizadoGastos(baseDatos){
+    visualizacionGastos.innerHTML = "";
+    baseDatos.forEach(el => {
+        let contenedorEvento = document.createElement("article");
+        contenedorEvento.innerHTML = `
+                <p>${el.titulo} ${dayjs(el.fecha).format('DD-MMMM-YYYY')} ----> ${el.gasto}$</p>
+
+                <button id=editar${el.id} class="editar" type=button >editar</button>
+                <button id=eliminar${el.id} class="eliminar" type=button >editar</button>
+                `
+        visualizacionGastos.appendChild(contenedorEvento)
+    }); 
+    let botonEditar = document.querySelectorAll(`editar`)
+    botonEditar.forEach((el)=>el.onclick = eventoEditar)
+}
+
+function eventoEditar(e){
+    alert(e.target.id)
+}
+renderizadoGastos(gastos)
 
 
 function crearEveto(){
-    gastos.push({id: gastos.length + 1, titulo: tituloGasto.value, fecha:ingresoFecha.value})
-    console.log(gastos)
-    alert(dayjs(gastos[gastos.length-1].fecha).format('DD-MMMM-YYYY'))
+    gastos.push({
+        id: gastos.length + 1, 
+        titulo: tituloGasto.value, 
+        gasto: montoGastado.value,
+        fecha:ingresoFecha.value})
+    console.log(gastos);
+    alert(dayjs(gastos[gastos.length-1].fecha).format('DD-MMMM-YYYY'));
     localStorage.setItem("gastosUsuario",JSON.stringify(gastos));
+    gastos.sort((a, b) => new Date(a.fecha)  - new Date(b.fecha));
+    renderizadoGastos(gastos)
 }
 
-gastos.sort((x, y) => y.fecha - x.fecha);
-console.log(gastos)
 
-gastos.sort((x, y) => x.precio - y.precio);
-console.log(gastos)
+gastos.sort((a, b) => new Date(a.fecha)  - new Date(b.fecha));
+
