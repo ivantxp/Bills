264239975d337d153c37963afb0324e392bf1c2b
Ivan Fuentes <ivantxp@gmail.com>
2023-02-27 08:54:37 -0300

    27-03

--- js/js.js
+++ js/js.js
@@ -1,20 +1,33 @@
 dayjs.locale("es");
 let gastos = [] ;
 let visualizacionGastos = document.getElementById("visualizacion_gastos");
+
 let tituloGasto= document.getElementById("titulo_gasto");
 let ingresoFecha = document.getElementById("fecha_ingreso");
-let credorGasto = document.getElementById("btn_crear");
+let detalleGasto = document.getElementById("detalle_gasto")
+let credorGasto = document.getElementById("boton_crear");
 credorGasto.onclick = crearEveto;
-
 let montoGastado = document.getElementById("monto_gastado");
 
-
-
-
-
-if( localStorage.getItem("gastosUsuario") != null){
+if( localStorage.getItem("gastosUsuario") != null){//control incial para cargar datos de gastos
     gastos = JSON.parse(localStorage.getItem("gastosUsuario"));
     console.log(gastos);
+}
+
+function controlIngresos(){
+    if(tituloGasto.value == "" ){
+        alert("Debe ingresar titulo del gasto")
+        return false
+    }else if(tituloGasto.length > 30){
+        alert("El titulo del gasto no puede tener mas de 30 caracteres")
+        return false
+    }else if(montoGastado < 0){
+        alert("el monto gastado no puede ser negativo")
+        return false
+    }else if(isNaN(montoGastado)){
+        alert("Tiene que ingresar numeros en mono gastado")
+    }
+    
 }
 
 function renderizadoGastos(baseDatos){
@@ -22,15 +35,17 @@ function renderizadoGastos(baseDatos){
     baseDatos.forEach(el => {
         let contenedorEvento = document.createElement("article");
         contenedorEvento.innerHTML = `
-                <p>${el.titulo} ${dayjs(el.fecha).format('DD-MMMM-YYYY')} ----> ${el.gasto}$</p>
-
+                <p>id:${el.id} ${el.titulo} ${dayjs(el.fecha).format('DD-MMMM-YYYY')} detalle: ${el.detalle} </p>
+                <p>Gastado: ${el.gasto}$</p>
                 <button id=editar${el.id} class="editar" type=button >editar</button>
-                <button id=eliminar${el.id} class="eliminar" type=button >editar</button>
-                `
+                <button id=eliminar${el.id} class="eliminar" type=button >Eliminar</button>
+            `
         visualizacionGastos.appendChild(contenedorEvento)
     }); 
-    let botonEditar = document.querySelectorAll(`editar`)
-    botonEditar.forEach((el)=>el.onclick = eventoEditar)
+    let botonEditar = document.querySelectorAll(`.editar`)//selecicono todas las clases editar
+    botonEditar.forEach((el)=>el.onclick = eventoEditar)//le pongo elvento onclick a cada boton editar
+    let botonEliminar = document.querySelectorAll(`.eliminar`)
+    botonEliminar.forEach((el)=>el.onclick = eventoEliminar)
 }
 
 function eventoEditar(e){
@@ -38,18 +53,25 @@ function eventoEditar(e){
 }
 renderizadoGastos(gastos)
 
-
+function eventoEliminar(e){
+    alert(e.target.id)
+}
 function crearEveto(){
     gastos.push({
         id: gastos.length + 1, 
         titulo: tituloGasto.value, 
         gasto: montoGastado.value,
-        fecha:ingresoFecha.value})
-    console.log(gastos);
-    alert(dayjs(gastos[gastos.length-1].fecha).format('DD-MMMM-YYYY'));
+        fecha: ingresoFecha.value,
+        detalle: detalleGasto.value
+    });
+    //console.log(gastos);
     localStorage.setItem("gastosUsuario",JSON.stringify(gastos));
     gastos.sort((a, b) => new Date(a.fecha)  - new Date(b.fecha));
     renderizadoGastos(gastos)
+    tituloGasto.value = "";
+    montoGastado.value = "";
+    ingresoFecha.value = "";
+    detalleGasto.value = "";
 }
 
 
